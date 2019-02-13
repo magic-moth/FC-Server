2efde1f06ae44e415c60e36d59b8fba8eebd873d
Steven Duncan <steven.k.duncan@gmail.com>
2019-02-13 15:28:59 -0600

    index.js

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
 const PORT = process.env.PORT || 3000;
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname + '/../FC-Client/dist'));
 
+// app.get('/', (req, res) => {
+// })
 
 app.listen(PORT, () => {
   console.log(`Server active on port: ${PORT}`);
